3a0b79acaf3207b23491a9fa9de4a4b842f63a80
Hugo McNally <hugo.mcnally@gmail.com>
2024-11-08 17:57:24 +0000

    Made the multiplier parametrisable

--- examples/virtual_cores/multiplier.sv
+++ examples/virtual_cores/multiplier.sv
@@ -1,110 +1,41 @@
 /*
-  Single-cycle unsigned multiplier
+ * A simple combinational unsigned multiplier
  */
-module multiplier (
-  input clk_i,
-  input rst_ni,
-
-  input  logic  [7:0] data_in1_i,
-  input  logic  [7:0] data_in2_i,
-  output logic [15:0] data_out_o,
-  output logic        data_out_valid_o
+module multiplier #(
+  parameter unsigned Width = 8
+) (
+  input  logic [ Width   -1:0] data_in1_i,
+  input  logic [ Width   -1:0] data_in2_i,
+  output logic [(Width*2)-1:0] data_out_o
 );
-  // Partial products
-  logic [7:0] [7:0] pp;
+  // Partial products.
+  logic [Width-1:0] pp[Width];
+
+  for (genvar idx = 0; idx < Width; ++idx) begin : gen_partial_products
+    assign pp[idx] =  data_in1_i & {Width{data_in2_i[idx]}};
+  end : gen_partial_products
+
   // Intermediate sum and carry.
-  // Unrolled to avoid UNOPTFLAT warnings from Verilator.
-  logic [7:0] s0;
-  logic c0;
-  logic [7:0] s1;
-  logic c1;
-  logic [7:0] s2;
-  logic c2;
-  logic [7:0] s3;
-  logic c3;
-  logic [7:0] s4;
-  logic c4;
-  logic [7:0] s5;
-  logic c5;
-  logic [7:0] s6;
-  logic c6;
-  // Final result
-  logic [15:0] result;
+  logic [Width-1:0] s[Width];
+  logic c[Width];
 
-  // Generate partial products
-  for (genvar ii = 0; ii < 8; ii++) begin : g_partial_products
-    assign pp[ii] = {8{data_in1_i[ii]}} & data_in2_i;
-  end
+  assign {c[0], s[0]} = {1'b0, pp[0]};
 
-  // Adders
-  adder #(
-    .Width(8)
-  ) u_adder_0 (
-    .in1_i ({1'b0, pp[0][7:1]}),
-    .in2_i (pp[1]),
-    .sum_o (s0),
-    .carry_o (c0)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_1 (
-    .in1_i ({c0, s0[7:1]}),
-    .in2_i (pp[2]),
-    .sum_o (s1),
-    .carry_o (c1)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_2 (
-    .in1_i ({c1, s1[7:1]}),
-    .in2_i (pp[3]),
-    .sum_o (s2),
-    .carry_o (c2)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_3 (
-    .in1_i ({c2, s2[7:1]}),
-    .in2_i (pp[4]),
-    .sum_o (s3),
-    .carry_o (c3)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_4 (
-    .in1_i ({c3, s3[7:1]}),
-    .in2_i (pp[5]),
-    .sum_o (s4),
-    .carry_o (c4)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_5 (
-    .in1_i ({c4, s4[7:1]}),
-    .in2_i (pp[6]),
-    .sum_o (s5),
-    .carry_o (c5)
-  );
-  adder #(
-    .Width(8)
-  ) u_adder_6 (
-    .in1_i ({c5, s5[7:1]}),
-    .in2_i (pp[7]),
-    .sum_o (s6),
-    .carry_o (c6)
-  );
+  for (genvar i = 0; i < Width-1; ++i) begin : adders
+    adder #(
+      .Width(8)
+    ) u_adder (
+      .in1_i ({c[i], s[i][Width-1:1]}),
+      .in2_i (pp[i+1]),
+      .sum_o (s[i+1]),
+      .carry_o (c[i+1])
+    );
+  end : adders
 
-  // Assemble final result
-  assign result = {c6, s6, s5[0], s4[0], s3[0], s2[0], s1[0], s0[0], pp[0][0]};
+  // Output from intermediates
+  assign data_out_o[(Width*2)-1:Width-1] = {c[Width-1], s[Width-1]};
 
-  // Register
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      data_out_o <= 16'd0;
-      data_out_valid_o <= 1'b0;
-    end else begin
-      data_out_o <= result;
-      data_out_valid_o <= 1'b1;
-    end
-  end
+  for (genvar idx = 0; idx < Width-1; ++idx) begin : gen_output_lower_bits
+    assign data_out_o[idx] = s[idx][0];
+  end : gen_output_lower_bits
 endmodule : multiplier
