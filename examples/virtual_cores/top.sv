3d57bcd3714eb0816c7075103ace8a6006d4626c
Elliot Baptist <elliot.baptist@lowrisc.org>
2024-11-08 15:39:00 +0000

    Improve multiplier testing

--- examples/virtual_cores/top.sv
+++ examples/virtual_cores/top.sv
@@ -5,7 +5,8 @@ module top (
 
     logic clk_i = 1'b1, rst_ni = 1'b0;
 
-    logic [7:0] data_in1_i, data_in2_i, data_out_o;
+    logic [7:0] data_in1_i, data_in2_i;
+    logic [15:0] data_out_o;
     logic data_out_valid_o;
 
     multiplier u_multiplier (
@@ -35,14 +36,38 @@ module top (
       data_in2_i = 7;
 
       #(2*HalfClockPeriod);
+      if (data_out_o != 14) $error("expected 14, got:", data_out_o);
+      if (!data_out_valid_o) $error("expected valid");
+
       data_in1_i = 211;
       data_in2_i = 98;
 
       #(2*HalfClockPeriod);
+      if (data_out_o != 20678) $error("expected 20678, got:", data_out_o);
+      if (!data_out_valid_o) $error("expected valid");
+
       data_in1_i = 123;
       data_in2_i = 77;
 
-      #(4*HalfClockPeriod);
+      #(2*HalfClockPeriod);
+      if (data_out_o != 9471) $error("expected 9471, got:", data_out_o);
+      if (!data_out_valid_o) $error("expected valid");
+
+      data_in1_i = 0;
+      data_in2_i = 0;
+
+      #(2*HalfClockPeriod);
+      if (data_out_o != 0) $error("expected 0, got:", data_out_o);
+      if (!data_out_valid_o) $error("expected valid");
+
+      data_in1_i = 255;
+      data_in2_i = 255;
+
+      #(2*HalfClockPeriod);
+      if (data_out_o != 65025) $error("expected 65025, got:", data_out_o);
+      if (!data_out_valid_o) $error("expected valid");
+
+      #(2*HalfClockPeriod);
       $finish;
     end : main
 
