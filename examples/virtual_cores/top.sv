f026023a542a673fa5f8c04ff4f9ba4024175cb9
Hugo McNally <hugo.mcnally@gmail.com>
2024-11-09 10:49:34 +0000

    Cleaned up top testbench

--- examples/virtual_cores/top.sv
+++ examples/virtual_cores/top.sv
@@ -1,12 +1,20 @@
-module top (
-   output logic modulated_o
-);
-    localparam HalfClockPeriod = 25ns; // 20MHz
+module top;
+    localparam real HalfClockPeriod = 25ns; // 20MHz
+    localparam int unsigned Width = 8;
+
+    typedef logic [    Width-1:0] multiplicand_t;
+    typedef logic [(Width*2)-1:0] product_t;
+
+    typedef struct {
+      multiplicand_t in1;
+      multiplicand_t in2;
+      product_t expected;
+    } test_t;
 
     logic clk_i = 1'b1;
 
-    logic [7:0] data_in1_i, data_in2_i;
-    logic [15:0] data_out_o;
+    multiplicand_t data_in1_i, data_in2_i;
+    product_t data_out_o;
 
     multiplier u_multiplier (
         .data_in1_i,
@@ -14,49 +22,38 @@ module top (
         .data_out_o
     );
 
-    // Toggle clock
-    always #(HalfClockPeriod) clk_i <= !clk_i;
-
-    initial begin : main
+    initial begin : setup_tracing
       string trace_file;
 
       if ($value$plusargs("trace=%s", trace_file)) begin
         $dumpfile(trace_file);
         $dumpvars;
       end
+    end : setup_tracing
 
+    // Toggle clock
+    always #(HalfClockPeriod) clk_i <= !clk_i;
+
+    test_t tests[] = '{
+      '{in1: 2, in2: 7, expected: 14},
+      '{in1: 211, in2: 98, expected: 20678},
+      '{in1: 123, in2: 77, expected: 9471},
+      '{in1: 0, in2: 0, expected: 0},
+      '{in1: 255, in2: 255, expected: 65025}
+    };
+    initial begin : main
       #(HalfClockPeriod);
-      data_in1_i = 2;
-      data_in2_i = 7;
 
-      #(2*HalfClockPeriod);
-      if (data_out_o != 14) $error("expected 14, got:", data_out_o);
+      foreach (tests[i]) begin : test_routine
+        data_in1_i = tests[i].in1;
+        data_in2_i = tests[i].in2;
 
-      data_in1_i = 211;
-      data_in2_i = 98;
+        #(2*HalfClockPeriod);
+        if (data_out_o != tests[i].expected) begin
+          $error("expected %d, got: %d", tests[i].expected, data_out_o);
+        end
+      end : test_routine
 
-      #(2*HalfClockPeriod);
-      if (data_out_o != 20678) $error("expected 20678, got:", data_out_o);
-
-      data_in1_i = 123;
-      data_in2_i = 77;
-
-      #(2*HalfClockPeriod);
-      if (data_out_o != 9471) $error("expected 9471, got:", data_out_o);
-
-      data_in1_i = 0;
-      data_in2_i = 0;
-
-      #(2*HalfClockPeriod);
-      if (data_out_o != 0) $error("expected 0, got:", data_out_o);
-
-      data_in1_i = 255;
-      data_in2_i = 255;
-
-      #(2*HalfClockPeriod);
-      if (data_out_o != 65025) $error("expected 65025, got:", data_out_o);
-
-      #(2*HalfClockPeriod);
       $finish;
     end : main
 
