77838828793abcb67364c99b305e248e843eb901
Hugo McNally <hugo.mcnally@gmail.com>
2024-11-11 16:47:46 +0000

    Added lint script

--- flake.nix
+++ flake.nix
@@ -31,17 +31,49 @@
           python = pkgs.python311;
           overrides = [poetry2nix.defaultPoetryOverrides];
         };
+
+      buildInputs = with pkgs; [libelf zlib];
+      nativeBuildInputs = with pkgs; [reuse graphviz verilator pythonEnv];
+
+      lints = pkgs.stdenv.mkDerivation {
+        name = "fusesoc-examples-lints";
+        src = ./.;
+        inherit buildInputs nativeBuildInputs;
+        dontBuild = true;
+        doCheck = true;
+        PYTHONPATH=./python_plugins;
+        checkPhase = ''
+          HOME=$TMPDIR
+
+          reuse lint
+          ruff format --check
+          ruff check
+
+          prims_checks() {
+            fusesoc run --setup hugom:example:top
+            fusesoc run --flag select_prims --flag prims_specific --setup hugom:example:top
+            fusesoc run --flag select_prims --flag prims_secret hugom:example:top
+          }
+          pushd prims_lib/virtual_cores
+            prims_checks
+          popd
+          pushd prims_lib/filters
+            prims_checks
+          popd
+        '';
+        installPhase = "mkdir $out";
+      };
     in {
       formatter = pkgs.alejandra;
       devShells.default = pkgs.mkShell {
         name = "prim-lib-examples";
-        buildInputs = with pkgs; [libelf zlib];
-        nativeBuildInputs = with pkgs; [graphviz verilator pythonEnv];
+        inherit buildInputs nativeBuildInputs;
         shellHook = ''
           export REPO_ROOT="$(${getExe pkgs.git} rev-parse --show-toplevel)"
           export PYTHONPATH="$REPO_ROOT/python_plugins";
         '';
       };
+      checks = {inherit lints;};
     };
   in
     flake-utils.lib.eachDefaultSystem system_outputs;
